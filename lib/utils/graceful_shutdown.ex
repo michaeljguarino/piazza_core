e8fe5379354227d6450d5dbd864ea1a8dfd5b7d3
Michael Guarino <mguarino46@gmail.com>
2020-02-14 10:30:32 -0500

    Add graceful shutdown genserver

---
+++ lib/utils/graceful_shutdown.ex
@@ -0,0 +1,30 @@
+defmodule Piazza.GracefulShutdown do
+  @moduledoc """
+  Genserver to handle graceful shutdowns of an application. Configure
+  with:
+
+  ```
+  config :piazza_core,
+    shutdown_delay: delay_in_ms
+  ```
+  And placd it at the beginning of you application supervision tree.
+  """
+  use GenServer
+  require Logger
+
+  @shutdown_delay Application.get_env(:piazza_core, :shutdown_delay) || 0
+
+  def start_link(args \\ :ok) do
+    GenServer.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(args) do
+    Process.flag(:trap_exit, true)
+    {:ok, args}
+  end
+
+  def terminate(_, _) do
+    Logger.info "Terminating after #{@shutdown_delay} ms"
+    :timer.sleep(@shutdown_delay)
+  end
+end
